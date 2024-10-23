fe895fb3b8474a338c5369939a2ff276f6c0a09a
hanulee0123 <119769753+hanulee0123@users.noreply.github.com>
2024-10-24 01:06:31 +0900

    Update layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import '../styles/globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: '九州合唱団',
+  description: '九州の歌声を世界へ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ja">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
