d571a5a1e475ea419046b030c443914e17f5b8f9
hanulee0123 <119769753+hanulee0123@users.noreply.github.com>
2024-10-24 23:53:08 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,43 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-      CHOIR_NAME: process.env.CHOIR_NAME || '九州合唱団',
-      FOUNDATION_DATE: process.env.FOUNDATION_DATE || '2024年4月1日',
-    },
-  }
-  
-module.exports = {
-    async headers() {
-      return [
-        {
-          // すべてのページにヘッダーを適用
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';",
-            },
-            {
-              key: 'Strict-Transport-Security',
-              value: 'max-age=63072000; includeSubDomains; preload',
-            },
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Referrer-Policy',
-              value: 'no-referrer',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: 'geolocation=(), microphone=()',
-            },
-          ],
-        },
-      ]
-    }}
+  env: {
+    CHOIR_NAME: process.env.CHOIR_NAME || '九州合唱団',
+    FOUNDATION_DATE: process.env.FOUNDATION_DATE || '2024年4月1日',
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline';",
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'no-referrer',
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=()',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, s-maxage=3600, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
